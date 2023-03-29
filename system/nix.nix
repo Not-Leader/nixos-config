53022975a7690dbc111a1cac0e23946fb0472172
not-leader <not-leader1@outlook.com>
2023-03-29 12:10:13 +0300

    add nix-community cache

--- system/nix.nix
+++ system/nix.nix
@@ -8,9 +8,11 @@
     substituters = [
       "https://cache.nixos.org"
       "https://helix.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
