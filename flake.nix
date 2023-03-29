dbe2b4e45a64dac6dd76e065fd783f3432168484
not-leader <not-leader1@outlook.com>
2023-03-29 12:20:50 +0300

    "move substituters to flake.nix file"

--- flake.nix
+++ flake.nix
@@ -1,5 +1,17 @@
 {
   description = "A very basic flake";
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://helix.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
