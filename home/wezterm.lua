54b3d453ff966c6978b77041d18955f95ff7408a
not-leader <not-leader1@outlook.com>
2023-03-30 19:22:17 +0300

    change wezterm theme to material darker

--- home/wezterm.lua
+++ home/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 return {
   font = wezterm.font 'Noto Sans Mono',
   font_size = 10.0,
-  color_scheme = 'Classic Dark (base16)',
+  color_scheme = 'MaterialDarker',
   enable_tab_bar = false;
 }
 
