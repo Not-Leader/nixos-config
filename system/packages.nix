7e1aa138dab3508b791d2202bccbb4043639cd1d
not-leader <not-leader1@outlook.com>
2023-03-30 15:08:50 +0300

    change greetd theme to adwaita dark

--- system/packages.nix
+++ system/packages.nix
@@ -9,7 +9,7 @@
   environment.systemPackages = with pkgs; [
     helix # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     bridge-utils
-    lounge-gtk-theme
+    gnome-themes-extra
     wget
   ];
 }
