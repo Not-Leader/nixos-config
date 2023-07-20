0c6d99a0890e1c381d135f394f516e587d9fad05
not-leader <not-leader1@outlook.com>
2023-07-20 15:31:18 +0300

    steam

--- system/default.nix
+++ system/default.nix
@@ -148,4 +148,12 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-run"
+  ];  
+
+  programs.steam.enable = true;
 }
